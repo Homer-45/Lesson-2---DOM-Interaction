c8aee7e5a35246fab6209e204e572e6f6c990204
Homer <honasanhomer@gmail.com>
2021-10-08 12:32:21 +0800

    Activity 4 - JS Data and Methods. Edit some errors at HTML

--- activity 4 - Dynamic Styling Problem/app.js
+++ activity 4 - Dynamic Styling Problem/app.js
@@ -1,5 +1,47 @@
 const app = Vue.createApp({    
-    
+    data() {
+        return {  
+            user: '',
+            user1: false,
+            user2: false,
+            enable: true,
+            color: '',
+            keyColor: '',
+
+           
+        };        
+    },
+    methods: {
+        Input(event){
+            this.user = event.target.value;
+
+            if(this.user === 'user1'){
+                this.user1 = !this.user1;
+            }else if(this.user ==='user2'){
+                this.user2 = !this.user2;
+            }else{
+                this.user1 = ''
+                this.user2 = ''
+                
+            }
+        },
+        toggleParagraph(){
+            this.enable = !this.enable;
+        },
+
+        PickColor(event){
+            this.color = event.target.value;
+            if(this.color === 'red'){
+                this.keyColor = this.color;
+            }else if(this.color === 'blue'){
+                this.keyColor = this.color
+            }else if(this.color === 'black'){
+                this.keyColor = this.color;
+            }else{
+                this.keyColor = ''
+            }
+        }
+    }
     
 });
 app.mount('#assignment');
